e6163e42a01ba0e539aa9848321586af3d233768
Thilo L. Fischer <thilo.leon.fischer@tuhh.de>
2019-02-17 00:23:35 +0100

    Update ok60 keymap.

--- keyboards/ok60/keymaps/inu7el/keymap.c
+++ keyboards/ok60/keymaps/inu7el/keymap.c
@@ -9,14 +9,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
         LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT,
-        KC_LALT, MO(1), KC_LGUI,               KC_SPC, KC_LGUI, MO(1), KC_RALT, BL_STEP
+        KC_LCTL, MO(1), KC_LGUI,               KC_SPC, KC_LALT, MO(1), KC_RALT, BL_STEP
     ),
 
     [1] = LAYOUT_60_ansi_split_bksp(
         _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, KC_MEDIA_REWIND, KC_AUDIO_VOL_UP, KC_AUDIO_VOL_DOWN, KC_MEDIA_FAST_FORWARD, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,
+        _______, KC_MEDIA_REWIND, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, KC_MEDIA_FAST_FORWARD, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______,                   _______,                            _______, _______, _______, _______
+        _______, _______, _______,                   _______,                            _______, _______, _______, BL_BRTG
     ),
 };
